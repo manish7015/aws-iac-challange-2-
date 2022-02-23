70709c5968cb92d247f74deff8dbc72b1c7b4e47
manish7015 <97020776+manish7015@users.noreply.github.com>
2022-02-23 17:10:17 +0000

    second commit

--- variable.tf
+++ variable.tf
@@ -9,20 +9,3 @@ variable "region" {
 variable "vpc_id" {
   default = "vpc-044306c8cbd56c315"
 }
-
-# Input vars AZs
-
-variable "az" {
-
-  type = map(any)
-  default = {
-    az-1 = "eu-west-1a"
-    az-2 = "eu-west-1b"
-    az-3 = "eu-west-1c"
-    az-4 = "eu-west-1d"
-    az-5 = "eu-west-1e"
-    az-6 = "eu-west-1f"
-
-  }
-
-}
