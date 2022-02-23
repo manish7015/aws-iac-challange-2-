7163eb0a90462d17d9ba8c3ad200f51cc1bbcfe4
manish7015 <97020776+manish7015@users.noreply.github.com>
2022-02-23 17:08:35 +0000

    Update main.tf
    second commit

--- main.tf
+++ main.tf
@@ -9,155 +9,22 @@ data "aws_subnets" "iac-2" {
   filter {
     name   = "vpc-id"
     values = [var.vpc_id]
-
   }
 
 }
 
 data "aws_subnet" "iac-2" {
-
   for_each = toset(data.aws_subnets.iac-2.ids)
   id       = each.value
 
 }
 
-# Filtering available subnets in AZ "az-1"
+# locals to store and map availability zone name to a list of one or more subnet ids
+locals {
+  availability_zone_subnets = {
+for s in data.aws_subnet.iac-2 : s.availability_zone => s.id...
 
-data "aws_subnets" "az-1" {
-
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
   }
-
-  filter {
-    name   = "availabilityZone"
-    values = [var.az["az-1"]]
-  }
-
-}
-
-data "aws_subnet" "az-1" {
-
-  for_each = toset(data.aws_subnets.az-1.ids)
-  id       = each.value
-
-}
-
-# Filtering available subnets in AZ "az-2"
-
-data "aws_subnets" "az-2" {
-
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-
-  filter {
-    name   = "availabilityZone"
-    values = [var.az["az-2"]]
-  }
-
-}
-
-data "aws_subnet" "az-2" {
-
-  for_each = toset(data.aws_subnets.az-2.ids)
-  id       = each.value
-
-}
-
-# Filtering available subnets in AZ "az-3"
-
-data "aws_subnets" "az-3" {
-
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-
-  filter {
-    name   = "availabilityZone"
-    values = [var.az["az-3"]]
-  }
-
-}
-
-data "aws_subnet" "az-3" {
-
-  for_each = toset(data.aws_subnets.az-3.ids)
-  id       = each.value
-
-}
-
-# Filtering available subnets in AZ "az-4"
-
-data "aws_subnets" "az-4" {
-
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-
-  filter {
-    name   = "availabilityZone"
-    values = [var.az["az-4"]]
-  }
-
-}
-
-data "aws_subnet" "az-4" {
-
-  for_each = toset(data.aws_subnets.az-4.ids)
-  id       = each.value
-
-}
-
-
-# Filtering available subnets in AZ "az-5"
-
-data "aws_subnets" "az-5" {
-
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-
-  filter {
-    name   = "availabilityZone"
-    values = [var.az["az-5"]]
-  }
-
-}
-
-data "aws_subnet" "az-5" {
-
-  for_each = toset(data.aws_subnets.az-5.ids)
-  id       = each.value
-
-}
-
-# Filtering available subnets in AZ "az-6"
-
-data "aws_subnets" "az-6" {
-
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-
-  filter {
-    name   = "availabilityZone"
-    values = [var.az["az-6"]]
-  }
-
-}
-
-data "aws_subnet" "az-6" {
-
-  for_each = toset(data.aws_subnets.az-6.ids)
-  id       = each.value
-
 }
 
 # provider with region 
