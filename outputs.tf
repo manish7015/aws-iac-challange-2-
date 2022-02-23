6a6e371609544134c491b548b565524516df3948
manish7015 <97020776+manish7015@users.noreply.github.com>
2022-02-23 17:41:01 +0000

    update output.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+# vpc ids in region
+output "vpc_ids_in_selected_region" { value = data.aws_vpcs.vpc_id.ids }
+
+# subnets ids in each Azs
 output "subnets_in_azs" {
   value = local.availability_zone_subnets
 }
